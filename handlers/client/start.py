af8401da02d4f2e1b6e983d38ea6d6f737aba8ff
TENNESSI <bratuhin-kostya99@mail.ru>
2025-08-07 16:19:00 +0400

    изменил импорт клавиатуры, добавил проверку isadmin
    Signed-off-by: TENNESSI <bratuhin-kostya99@mail.ru>

--- handlers/client/start.py
+++ handlers/client/start.py
@@ -1,15 +1,16 @@
 from aiogram import Router, types
 from aiogram.filters import Command
-from keyboards.client import main_kb
+from keyboards.client import main_kb as start_client_kb
 
 router = Router()
 
 @router.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer(
-        "Добро пожаловать в барбершоп!",
-        reply_markup=main_kb()
-    )
+async def start(message: types.Message, is_admin: bool):
+    if not is_admin:
+        await message.answer(
+            "Добро пожаловать в барбершоп!",
+            reply_markup=start_client_kb()
+        )
 
 @router.message(lambda message: message.text == "📌 Записаться")
 async def book_service(message: types.Message):
